e3aed3885036066216bfc776cb5051d3c8b784dd
YanaVeryutina <veryutina.yana@yandex.ru>
2021-12-24 16:00:34 +0300

    refactored homework

--- src/test/java/guru/qa/tests/StudentRegistrationForm.java
+++ src/test/java/guru/qa/tests/StudentRegistrationForm.java
@@ -14,28 +14,25 @@ public class StudentRegistrationForm {
     @Test
     void fillFormTest() {
         open("https://demoqa.com/automation-practice-form");
-        //$(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
 
         $("[id=firstName]").setValue("Yana");
         $("#lastName").setValue("Veryutina");
         $("#userEmail").setValue("test@test.com");
         $("[for=gender-radio-2]").click();
-        //$("#genterWrapper").$(byText("Male")).click(); сработал первый вариант
+
         $("#userNumber").setValue("1234567890");
 
         $("#dateOfBirthInput").click();
-        //Перед вводом даты нужно очистить поле?
-        //$("#dateOfBirthInput").setValue("06 Jan 1997");
+
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("1997");
         $(".react-datepicker__day--006:not(.react-datepicker__day--outside-month)").click();
 
-        //$("#subjectsContainer").click();
+
         $("#subjectsInput").setValue("Automation");
         $("[for=hobbies-checkbox-3]").click();
 
         $("#uploadPicture").uploadFile(new File("src/test/resources/img/123456.png"));
-        $("#uploadPicture").uploadFromClasspath("img/123456.png");
 
         $("#currentAddress").setValue("Something");
 
